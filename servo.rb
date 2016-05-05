a93075ccf56eebba2bdddf013a632fe8df6f90fc
kento sasamoto <knote0721@gmail.com>
2016-05-05 10:00:04 +0900

    door

---
+++ servo.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'arduino_firmata'
+get '/' do
+  arduino = ArduinoFirmata.connect ARGV.shift
+  5.times do
+    angle = rand 180
+    puts angle
+    arduino.servo_write 13, angle
+    sleep 1
+  end
+end
+
+get '/open' do
+  arduino = ArduinoFirmata.connect ARGV.shift
+  angle = 180
+  puts 'open'
+  arduino.servo_write 13, angle
+  "open door"
+end
+
+get '/close' do
+  arduino = ArduinoFirmata.connect ARGV.shift
+  angle = 0
+  puts 'close'
+  arduino.servo_write 13, angle
+  "close door"
+end
+
+get '/exit' do
+ Process.kill('TERM', Process.pid)
+end
